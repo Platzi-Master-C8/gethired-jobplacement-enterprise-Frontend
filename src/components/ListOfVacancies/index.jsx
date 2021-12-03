53a75acdd0a36f2e78573242075d507ab90b1212
Leonardo Espinoza <leoespinozidk@gmail.com>
2021-12-03 02:08:56 -0600

    feat: add styles and more vacancy component

--- src/components/ListOfVacancies/index.jsx
+++ src/components/ListOfVacancies/index.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Vacancy } from '../Vacancy';
+
+import { Title } from './styles';
 
 export const ListOfVacancies = () => {
     return (
-        <Vacancy
-            title="Job title"
-            date={new Date()}
-            salary={{ min: 500, max: 1000 }}
-            modality="remote"
-            applies={100}
-            seen={300}
-            description="Lorem ipsum"
-        />
+        <Fragment>
+            <Title>History</Title>
+            <Vacancy
+                title="Job title"
+                date={new Date()}
+                salary={{ min: 500, max: 1000 }}
+                modality="remote"
+                applies={100}
+                seen={300}
+                description="Lorem ipsum"
+            />
+            <Vacancy
+                title="Job title"
+                date={new Date()}
+                salary={{ min: 500, max: 1000 }}
+                modality="remote"
+                applies={100}
+                seen={300}
+                description="Lorem ipsum"
+            />
+            <Vacancy
+                title="Job title"
+                date={new Date()}
+                salary={{ min: 500, max: 1000 }}
+                modality="remote"
+                applies={100}
+                seen={300}
+                description="Lorem ipsum"
+            />
+        </Fragment>
     );
 };
