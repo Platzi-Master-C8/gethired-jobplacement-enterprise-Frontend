4400109d3ae2bc7334ec6d1c5aaada1b9dd0aa6c
Fernando De Leon <gasnare@hotmail.com>
2021-12-02 23:08:11 -0500

    feat: page vacancies

---
+++ src/pages/vacancies.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header/header';
+
+const Vacancies = () => {
+    return (
+        <div className="vacancies">
+            <Header />
+        </div>
+    );
+};
+
+export default Vacancies;
