161245b83756fa47b49ba6290e103965859484d0
Leonardo Espinoza <leoespinozidk@gmail.com>
2021-12-03 02:11:53 -0600

    feat: fix structure and styling component

--- src/components/Vacancy/index.jsx
+++ src/components/Vacancy/index.jsx
@@ -1,51 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// import MenuItem from '@mui/material/MenuItem'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Button from '@mui/material/Button';
+import { Vacancy as Container, Header, Div, DivRight, Tag, Salary, TagContainer, Title, Footer } from './styles';
 
-export const Vacancy = ({ title, date, salary, modality, applies, seen, description }) => {
+export const Vacancy = ({ title, salary, modality, applies, seen, description }) => {
     return (
-        <div>
+        <Container>
+            <Header>
+                <Div>
+                    <Title>{title}</Title>
+                    <TagContainer>
+                        <Tag>{applies} applied </Tag> <Tag>{seen} seen</Tag>
+                    </TagContainer>
+                </Div>
+                <DivRight>
+                    <Salary>
+                        ${salary.min} - ${salary.max}
+                    </Salary>
+                    <Tag>{modality}</Tag>
+                </DivRight>
+            </Header>
             <div>
-                <div>
-                    <div>
-                        <h3>{title}</h3>
-                        <p>{date.day} yes</p>
-                    </div>
-                    <div>
-                        <span>{applies} applied </span> <span>{seen} seen</span>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <p>
-                            ${salary.min} - ${salary.max}
-                        </p>
-                    </div>
-                    <div>
-                        <span>{modality}</span>
-                    </div>
-                </div>
+                <p>
+                    {description} lorem Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de
+                    texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde. Lorem Ipsum es
+                    simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto
+                    de relleno estándar de las industrias desde. Lorem Ipsum es simplemente el texto de relleno de las
+                    imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias
+                    desde.Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem
+                    Ipsum ha sido el texto de relleno estándar de las industrias desde
+                </p>
             </div>
-            <div>
-                <p>{description}</p>
-            </div>
-            <div>
-                <select>
-                    <option value="edit">Edit</option>
-                    <option value="delete">Delete</option>
-                </select>
-                <button type="button" variant="contained">
+            <Footer>
+                <FormControl
+                    sx={{
+                        width: 100,
+                    }}
+                >
+                    <InputLabel>Options</InputLabel>
+                    <Select label="Options">
+                        <MenuItem value="edit">Edit</MenuItem>
+                        <MenuItem value="delete">Delete</MenuItem>
+                    </Select>
+                </FormControl>
+                <Button type="button" variant="outlined">
                     See more
-                </button>
-            </div>
-        </div>
+                </Button>
+            </Footer>
+        </Container>
     );
 };
 
 Vacancy.propTypes = {
     title: PropTypes.string.isRequired,
-    date: PropTypes.instanceOf(new Date()).isRequired,
     salary: PropTypes.shape({
         min: PropTypes.number.isRequired,
         max: PropTypes.number.isRequired,
--- src/components/Vacancy/styles.js
+++ src/components/Vacancy/styles.js
@@ -1,6 +1,57 @@
 import styled from 'styled-components';
 
-export const Vacancy = styled.div`
+export const Vacancy = styled.section`
     width: 100%;
+    max-width: 1048px;
     padding: 10px;
+    border-radius: 18px;
+    margin: 0 auto 15px;
+`;
+
+export const Header = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+`;
+
+export const Div = styled.div`
+    width: 50%;
+`;
+
+export const DivRight = styled.div`
+    width: 50%;
+    display: grid;
+    justify-items: right;
+`;
+
+export const Salary = styled.p`
+    margin: 0;
+    font-size: 24px;
+`;
+
+export const TagContainer = styled.div`
+    display: flex;
+    width: 250px;
+    justify-content: space-around;
+`;
+
+export const Tag = styled.div`
+    width: 100px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 30px;
+    border: 1px solid black;
+`;
+
+export const Footer = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+`;
+
+export const Title = styled.h3`
+    font-size: 24px;
+    margin: 0;
 `;
