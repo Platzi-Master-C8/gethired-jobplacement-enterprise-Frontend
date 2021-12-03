36bc6d49dde09770483ba61ae2e85880593c1e32
Fernando De Leon <gasnare@hotmail.com>
2021-12-02 23:05:45 -0500

    feat: add route to vacancies page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
 
 import Theme from 'Pages/Theme';
 import NotFound from 'Pages/NotFound';
+import Vacancies from '../pages/vacancies';
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route path="*" element={<NotFound />} />
             <Route path="/" element={<Theme />} />
+            <Route path="/Vacancies" element={<Vacancies />} />
         </Switch>
     </BrowserRouter>
 );
