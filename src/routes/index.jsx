0012c46e60ddb7597b82298a726717b75f680e6a
Leonardo Espinoza <leoespinozidk@gmail.com>
2021-12-06 13:27:32 -0600

    feat: change main route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
 
-import Theme from 'Pages/Theme';
 import NotFound from 'Pages/NotFound';
 import { CreateVacancies } from 'Pages/CreateVacancies';
 import { EditVacancies } from 'Pages/EditVacancies';
@@ -10,11 +9,10 @@ import VacanciesHistory from '../pages/VacanciesHistory';
 const Routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route path="/" element={<Theme />} />
+            <Route path="/" element={<VacanciesHistory />} />
             <Route path="/vacancies" element={<CreateVacancies />} />
             <Route path="/edit-vacancies" element={<EditVacancies />} />
             <Route path="*" element={<NotFound />} />
-            <Route path="/vacancies-history" element={<VacanciesHistory />} />
         </Switch>
     </BrowserRouter>
 );
