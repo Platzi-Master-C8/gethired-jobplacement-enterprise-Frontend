58853da4fefe10e3ae97e13da1cbb052968068b7
Fernando De Leon <gasnare@hotmail.com>
2021-12-11 10:55:53 -0500

    feat: add header with not login to test

--- src/pages/ListCandidatesVacancies.jsx
+++ src/pages/ListCandidatesVacancies.jsx
@@ -2,10 +2,12 @@ import { Container } from '@mui/material';
 import React from 'react';
 
 import { CardApplicantList } from '../components/CardApplicantList/CardApplicantList';
+import { HeaderNotLogin } from '../components/Header/HeaderNotLogin';
 
 export const ListCandidatesVacancies = () => {
     return (
         <Container>
+            <HeaderNotLogin />
             <CardApplicantList />
             <CardApplicantList />
             <CardApplicantList />
