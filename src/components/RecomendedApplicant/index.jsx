3f2c5610070167301ab88bc6eb6987d66f051255
Leonardo Espinoza <leoespinozidk@gmail.com>
2021-12-12 21:26:11 -0600

    feat: add styles to recommended and applicant comp

--- src/components/RecomendedApplicant/index.jsx
+++ src/components/RecomendedApplicant/index.jsx
@@ -1,24 +1,30 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
-export const RecommendedApplicant = ({ img, name, description, percentage, matchType }) => {
+import { Container, Title, Div, Details, Tag, MatchTag } from './styles';
+
+export const RecommendedApplicant = ({ name, description, percentage, matchType }) => {
     return (
         <Fragment>
-            <h2>Recommended</h2>
-            <div>
-                <img src={img} alt={name} />
-                <div>
-                    <p>{description}</p>
-                </div>
-                <span>{percentage} %</span>
-                <span>{matchType} match</span>
-            </div>
+            <Title>Recommended</Title>
+            <Container>
+                {/* <img src={img} alt={name} /> */}
+                <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5, fontSize: 60 }} />
+                <Div>
+                    <p>{name} Name </p>
+                    <Details>
+                        <p>{description} Lorem ipsum</p>
+                        <Tag>{percentage} 85%</Tag>
+                        <MatchTag>{matchType} great match</MatchTag>
+                    </Details>
+                </Div>
+            </Container>
         </Fragment>
     );
 };
 
 RecommendedApplicant.propTypes = {
-    img: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     percentage: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
