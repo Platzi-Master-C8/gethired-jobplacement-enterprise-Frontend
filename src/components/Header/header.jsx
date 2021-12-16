bafe9fc6a078271abe481364a6b13d49d0df8c2d
Fernando De Leon <gasnare@hotmail.com>
2021-12-15 21:39:26 -0500

    feat: new header by demo day of platzi

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,64 +1,45 @@
 import React from 'react';
 
 import logo from '../../img/gethired_logo.svg';
-import iconMessage from '../../img/icon_message.svg';
-import iconNotifications from '../../img/icon-notifications.svg';
-import iconVacancies from '../../img/icon-briefcase.png';
-import iconInterviews from '../../img/icon-interviews.png';
-import iconPostulations from '../../img/icon-postulations.png';
-import iconContacts from '../../img/icon-contacts.png';
+import iconSalaries from '../../img/salaries.png';
+import iconStar from '../../img/star.png';
+import iconCompany from '../../img/company.png';
 import 'Styles/header.scss';
 
 const Header = () => {
     return (
-        <div className="header-menu">
+        <header className="header-menu">
             <figure className="logo-gethired">
                 <img src={logo} alt="Logo Gethired" />
             </figure>
-            <ul>
-                <li className="icon-message">
-                    <a href="./">
-                        <img src={iconMessage} alt="Icon Message" />
-                        <p>Message</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./">
-                        <img src={iconNotifications} alt="Icon Notifications" />
-                        <p>Notifications</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./">
-                        <img src={iconVacancies} alt="Icon Forum" />
-                        <p>Vacancies</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./">
-                        <img src={iconInterviews} alt="Icon Interviews" />
-                        <p>Interviews</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./">
-                        <img src={iconPostulations} alt="Icon Postulations" />
-                        <p>Postulations</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./">
-                        <img src={iconContacts} alt="Icon Contacts" />
-                        <p>Contacts</p>
-                    </a>
-                </li>
-                <li className="icon-profile">
-                    <a className="icon-profile__user" href="./">
-                        {/* this will change later */}U
-                    </a>
-                </li>
-            </ul>
-        </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="./">
+                            <img className="icons-menu" src={iconSalaries} alt="Icon Interviews" />
+                            <p>Salaries</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./">
+                            <img className="icons-menu" src={iconStar} alt="Icon Postulations" />
+                            <p>Ratings</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./">
+                            <img className="icons-menu" src={iconCompany} alt="Icon Contacts" />
+                            <p>Company</p>
+                        </a>
+                    </li>
+                    <li className="icon-profile">
+                        <a className="icon-profile__user" href="./">
+                            P
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 };
 
