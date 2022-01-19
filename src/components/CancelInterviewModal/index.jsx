09d01582b05c3f7054c1b780eb1b91de1a6dd058
Zajith Corro <zajithcorrov@hotmail.com>
2022-01-18 21:49:29 -0600

    feat: deleting logic to open the modal
    deleting logic from the component and receive from props

--- src/components/CancelInterviewModal/index.jsx
+++ src/components/CancelInterviewModal/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import Button from '@mui/material/Button';
+import PropTypes from 'prop-types';
+
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
@@ -10,57 +11,44 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import { FormButton } from '../FormVacancies/styles';
 
-const style = {
-    fontWeight: 'bold',
-};
-
-export const CancelInterview = () => {
-    const [open, setOpen] = useState(false);
+export const CancelInterview = ({ isOpen, onClose }) => {
     const [value, setValue] = useState();
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
 
     const handleChange = (event) => {
         setValue(event.target.value);
     };
 
     return (
-        <div>
-            <Button variant="outlined" onClick={handleClickOpen}>
-                Open form dialog
-            </Button>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle sx={style}>Why are you going to cancel the interview?</DialogTitle>
-                <DialogContent>
-                    <FormControl component="fieldset">
-                        <RadioGroup
-                            aria-label="cancel-interview"
-                            name="radio-buttons-group"
-                            value={value}
-                            onChange={handleChange}
-                        >
-                            <FormControlLabel value="closed" control={<Radio />} label="Vacancy closed" />
-                            <FormControlLabel value="cancelled" control={<Radio />} label="Vacancy cancelled" />
-                            <FormControlLabel value="delayed" control={<Radio />} label="Vacancy delayed" />
-                            <FormControlLabel value="other" control={<Radio />} label="Other reason" />
-                        </RadioGroup>
-                    </FormControl>
-                </DialogContent>
-                <DialogActions>
-                    <FormButton variant="contained" color="error" onClick={handleClose}>
-                        Cancel
-                    </FormButton>
-                    <FormButton variant="contained" color="primary" type="submit" onClick={handleClose}>
-                        Send {'>'}
-                    </FormButton>
-                </DialogActions>
-            </Dialog>
-        </div>
+        <Dialog open={isOpen} onClose={onClose}>
+            <DialogTitle sx={{ fontWeight: 'bold' }}>Why are you going to cancel the interview?</DialogTitle>
+            <DialogContent>
+                <FormControl component="fieldset">
+                    <RadioGroup
+                        aria-label="cancel-interview"
+                        name="radio-buttons-group"
+                        value={value}
+                        onChange={handleChange}
+                    >
+                        <FormControlLabel value="closed" control={<Radio />} label="Vacancy closed" />
+                        <FormControlLabel value="cancelled" control={<Radio />} label="Vacancy cancelled" />
+                        <FormControlLabel value="delayed" control={<Radio />} label="Vacancy delayed" />
+                        <FormControlLabel value="other" control={<Radio />} label="Other reason" />
+                    </RadioGroup>
+                </FormControl>
+            </DialogContent>
+            <DialogActions>
+                <FormButton variant="contained" color="error" onClick={onClose}>
+                    Cancel
+                </FormButton>
+                <FormButton variant="contained" color="primary" type="submit" onClick={onClose}>
+                    Send {'>'}
+                </FormButton>
+            </DialogActions>
+        </Dialog>
     );
+};
+
+CancelInterview.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
 };
