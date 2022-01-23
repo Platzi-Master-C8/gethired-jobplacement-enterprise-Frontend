a46ef57fcb7dc357c20044511f81d86b0aa5e4e0
Leonardo Espinoza <leoespinozidk@gmail.com>
2022-01-23 00:24:58 -0600

    feat: create login and register buttons

---
+++ src/components/AuthButtons/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+import Button from '@mui/material/Button';
+
+export const LoginButton = () => {
+    const { loginWithReact } = useAuth0();
+
+    return (
+        <Button type="button" onClick={() => loginWithReact()}>
+            Log in
+        </Button>
+    );
+};
+
+export const SignupButton = () => {
+    const { loginWithReact } = useAuth0();
+
+    return (
+        <Button
+            type="button"
+            onClick={() =>
+                loginWithReact({
+                    screen_hint: 'signup',
+                })
+            }
+        >
+            Sign up
+        </Button>
+    );
+};
