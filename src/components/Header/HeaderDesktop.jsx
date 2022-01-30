3fe8623e0b2a268c38842b4e54f3e65eb1f48d00
Fernando De Leon <gasnare@hotmail.com>
2022-01-30 00:06:28 -0500

    refactor: make header independent for desktop

---
+++ src/components/Header/HeaderDesktop.jsx
@@ -0,0 +1,138 @@
+import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import { Menu, MenuList, MenuItem, Link, ListItemIcon, ListItemText, Box, Button } from '@mui/material';
+import { Message, BriefCase, Notification, Blog, UserGroup, Currency } from '@master-c8/icons';
+import { grey } from '@mui/material/colors';
+
+export const HeaderDesktop = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+        <MenuList sx={{ display: 'flex' }}>
+            <MenuItem>
+                <Link
+                    sx={{
+                        flexDirection: { sm: 'row', md: 'column' },
+                        display: 'flex',
+                        textDecoration: 'none',
+                        alignItems: 'center',
+                    }}
+                    size="small"
+                    href="/"
+                >
+                    <ListItemIcon>
+                        <Message sx={{ color: grey[900] }} />
+                    </ListItemIcon>
+                    <ListItemText sx={{ color: 'black' }}>Message</ListItemText>
+                </Link>
+            </MenuItem>
+            <MenuItem>
+                <Link
+                    sx={{
+                        flexDirection: { sm: 'row', md: 'column' },
+                        display: 'flex',
+                        textDecoration: 'none',
+                        alignItems: 'center',
+                    }}
+                    size="small"
+                    href="/"
+                >
+                    <ListItemIcon>
+                        <Notification sx={{ color: grey[900] }} />
+                    </ListItemIcon>
+                    <ListItemText sx={{ color: 'black' }}>Notifications</ListItemText>
+                </Link>
+            </MenuItem>
+            <MenuItem>
+                <Link
+                    sx={{
+                        flexDirection: { sm: 'row', md: 'column' },
+                        display: 'flex',
+                        textDecoration: 'none',
+                        alignItems: 'center',
+                    }}
+                    size="small"
+                    href="/"
+                >
+                    <ListItemIcon>
+                        <BriefCase sx={{ color: grey[900] }} />
+                    </ListItemIcon>
+                    <ListItemText sx={{ color: 'black' }}>Vacancies</ListItemText>
+                </Link>
+            </MenuItem>
+            <MenuItem>
+                <Link
+                    sx={{
+                        flexDirection: { sm: 'row', md: 'column' },
+                        display: 'flex',
+                        textDecoration: 'none',
+                        alignItems: 'center',
+                    }}
+                    size="small"
+                    href="/#/interviews"
+                >
+                    <ListItemIcon>
+                        <UserGroup sx={{ color: grey[900] }} />
+                    </ListItemIcon>
+                    <ListItemText sx={{ color: 'black' }}>Interviews</ListItemText>
+                </Link>
+            </MenuItem>
+            <MenuItem>
+                <Link
+                    sx={{
+                        flexDirection: { sm: 'row', md: 'column' },
+                        display: 'flex',
+                        textDecoration: 'none',
+                        alignItems: 'center',
+                    }}
+                    size="small"
+                    href="/"
+                >
+                    <ListItemIcon>
+                        <Blog sx={{ color: grey[900] }} />
+                    </ListItemIcon>
+                    <ListItemText sx={{ color: 'black' }}>Postulations</ListItemText>
+                </Link>
+            </MenuItem>
+            <MenuItem>
+                <Link
+                    sx={{
+                        flexDirection: { sm: 'row', md: 'column' },
+                        display: 'flex',
+                        textDecoration: 'none',
+                        alignItems: 'center',
+                    }}
+                    size="small"
+                    href="/"
+                >
+                    <ListItemIcon>
+                        <Currency sx={{ color: grey[900] }} />
+                    </ListItemIcon>
+                    <ListItemText sx={{ color: 'black' }}>Calculator</ListItemText>
+                </Link>
+            </MenuItem>
+            <Box>
+                <ListItemIcon
+                    onClick={handleClick}
+                    sx={{ display: 'flex', alignItems: 'center', color: 'black' }}
+                    size="large"
+                >
+                    <Avatar sx={{ bgcolor: '#AE4EFF', width: 45, height: 45, ml: 3 }} alt="Profile" />
+                </ListItemIcon>
+                <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+                    <MenuItem>
+                        <Button variant="contained" size="large" type="button">
+                            Sign out
+                        </Button>
+                    </MenuItem>
+                </Menu>
+            </Box>
+        </MenuList>
+    );
+};
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,130 +1,19 @@
 import React, { useState } from 'react';
 
 import { Header } from '@master-c8/commons';
-import Avatar from '@mui/material/Avatar';
-import { MenuList, MenuItem, Link, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
-import { Message, BriefCase, Notification, Blog, UserGroup, Website } from '@master-c8/icons';
-import { grey } from '@mui/material/colors';
+import { useMediaQuery, useTheme } from '@mui/material';
 
 import { HeaderMobile } from './HeaderMobile';
+import { HeaderDesktop } from './HeaderDesktop';
 
 export const HeaderEnterprise = () => {
     const [openDrawer, setOpenDrawer] = useState(false);
     const theme = useTheme();
     const isMatch = useMediaQuery(theme.breakpoints.down('md'));
+
     return (
         <Header className="header-menu" isLogged>
-            {isMatch ? (
-                <HeaderMobile openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} />
-            ) : (
-                <MenuList sx={{ display: 'flex' }}>
-                    <MenuItem>
-                        <Link
-                            sx={{
-                                flexDirection: { sm: 'row', md: 'column' },
-                                display: 'flex',
-                                textDecoration: 'none',
-                                alignItems: 'center',
-                            }}
-                            size="small"
-                            href="/"
-                        >
-                            <ListItemIcon>
-                                <Message sx={{ color: grey[900] }} />
-                            </ListItemIcon>
-                            <ListItemText sx={{ color: 'black' }}>Message</ListItemText>
-                        </Link>
-                    </MenuItem>
-                    <MenuItem>
-                        <Link
-                            sx={{
-                                flexDirection: { sm: 'row', md: 'column' },
-                                display: 'flex',
-                                textDecoration: 'none',
-                                alignItems: 'center',
-                            }}
-                            size="small"
-                            href="/"
-                        >
-                            <ListItemIcon>
-                                <Notification sx={{ color: grey[900] }} />
-                            </ListItemIcon>
-                            <ListItemText sx={{ color: 'black' }}>Notifications</ListItemText>
-                        </Link>
-                    </MenuItem>
-                    <MenuItem>
-                        <Link
-                            sx={{
-                                flexDirection: { sm: 'row', md: 'column' },
-                                display: 'flex',
-                                textDecoration: 'none',
-                                alignItems: 'center',
-                            }}
-                            size="small"
-                            href="/"
-                        >
-                            <ListItemIcon>
-                                <BriefCase sx={{ color: grey[900] }} />
-                            </ListItemIcon>
-                            <ListItemText sx={{ color: 'black' }}>Vacancies</ListItemText>
-                        </Link>
-                    </MenuItem>
-                    <MenuItem>
-                        <Link
-                            sx={{
-                                flexDirection: { sm: 'row', md: 'column' },
-                                display: 'flex',
-                                textDecoration: 'none',
-                                alignItems: 'center',
-                            }}
-                            size="small"
-                            href="/#/interviews"
-                        >
-                            <ListItemIcon>
-                                <UserGroup sx={{ color: grey[900] }} />
-                            </ListItemIcon>
-                            <ListItemText sx={{ color: 'black' }}>Interviews</ListItemText>
-                        </Link>
-                    </MenuItem>
-                    <MenuItem>
-                        <Link
-                            sx={{
-                                flexDirection: { sm: 'row', md: 'column' },
-                                display: 'flex',
-                                textDecoration: 'none',
-                                alignItems: 'center',
-                            }}
-                            size="small"
-                            href="/"
-                        >
-                            <ListItemIcon>
-                                <Blog sx={{ color: grey[900] }} />
-                            </ListItemIcon>
-                            <ListItemText sx={{ color: 'black' }}>Postulations</ListItemText>
-                        </Link>
-                    </MenuItem>
-                    <MenuItem>
-                        <Link
-                            sx={{
-                                flexDirection: { sm: 'row', md: 'column' },
-                                display: 'flex',
-                                textDecoration: 'none',
-                                alignItems: 'center',
-                            }}
-                            size="small"
-                            href="/"
-                        >
-                            <ListItemIcon>
-                                <Website sx={{ color: grey[900] }} />
-                            </ListItemIcon>
-                            <ListItemText sx={{ color: 'black' }}>Contacts</ListItemText>
-                        </Link>
-                    </MenuItem>
-                    <ListItemIcon sx={{ display: 'flex', alignItems: 'center', color: 'black' }} size="large">
-                        <Avatar sx={{ bgcolor: '#AE4EFF', width: 45, height: 45, ml: 3 }} alt="Profile" />
-                    </ListItemIcon>
-                </MenuList>
-            )}
+            {isMatch ? <HeaderMobile openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} /> : <HeaderDesktop />}
         </Header>
     );
 };
