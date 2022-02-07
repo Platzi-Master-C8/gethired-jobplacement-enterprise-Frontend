336b87ab38fc82d92a7c770c02e959b1b5dd12c2
Zajith Corro <zajithcorrov@hotmail.com>
2022-02-07 01:21:56 -0600

    feat: valide fields with react hook form

--- src/components/Commons/InputText/index.jsx
+++ src/components/Commons/InputText/index.jsx
@@ -5,27 +5,31 @@ import TextField from '@mui/material/TextField';
 
 import { Controller } from 'react-hook-form';
 
-export const InputText = ({ name, control, label, defaultValue, required, rows, multiline }) => {
+export const InputText = ({ name, control, label, required, rows, multiline, helperText }) => {
     return (
         <Controller
             name={name}
             control={control}
-            defaultValue={defaultValue}
-            render={({ field: { onChange, value }, fieldState: { error } }) => (
+            render={({ field: { value, ref, onChange }, fieldState: { invalid, error } }) => (
                 <TextField
-                    id={name}
-                    helperText={error ? error.message : null}
-                    error={!!error}
-                    onChange={onChange}
                     value={value}
-                    fullWidth
+                    inputRef={ref}
+                    onChange={onChange}
+                    error={invalid}
+                    helperText={error && error.message}
                     label={label}
-                    variant="filled"
-                    required={required}
                     rows={rows}
                     multiline={multiline}
+                    variant="filled"
+                    fullWidth
                 />
             )}
+            rules={{
+                required: {
+                    value: required,
+                    message: helperText,
+                },
+            }}
         />
     );
 };
@@ -33,16 +37,16 @@ export const InputText = ({ name, control, label, defaultValue, required, rows, 
 InputText.propTypes = {
     name: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
-    defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     required: PropTypes.bool,
+    helperText: PropTypes.string,
     rows: PropTypes.number,
     multiline: PropTypes.bool,
     control: PropTypes.shape({}).isRequired,
 };
 
 InputText.defaultProps = {
-    defaultValue: '',
     required: false,
     rows: 1,
     multiline: false,
+    helperText: 'Please enter the value',
 };
