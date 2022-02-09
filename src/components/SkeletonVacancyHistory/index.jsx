ff32b5fcfcbfe7490a67c1b41c2bb80c12084179
Fernando De Leon <gasnare@hotmail.com>
2022-02-09 14:39:32 -0500

    feat: loading vacancy history

---
+++ src/components/SkeletonVacancyHistory/index.jsx
@@ -0,0 +1,51 @@
+import React, { Fragment } from 'react';
+
+import Skeleton from '@mui/material/Skeleton';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+
+const LoadingVacancyHistory = () => {
+    return (
+        <Container sx={{ my: 5 }}>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Skeleton variant="text" width={100} height={40} />
+                <Skeleton variant="text" width={200} height={40} />
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-around', width: '20%' }}>
+                    <Skeleton variant="text" width={50} height={50} />
+                    <Skeleton variant="text" width={80} height={50} />
+                </Box>
+                <Skeleton variant="text" width={150} height={50} />
+            </Box>
+            <Box>
+                <Skeleton />
+                <Skeleton animation="wave" />
+                <Skeleton animation={false} />
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Skeleton variant="rectangular" width={80} height={30} />
+                <Skeleton variant="rectangular" width={80} height={30} />
+            </Box>
+            <Divider sx={{ mt: 2 }} />
+        </Container>
+    );
+};
+
+const SkeletonVacancyHistory = () => {
+    return (
+        <Fragment>
+            <LoadingVacancyHistory />
+            <LoadingVacancyHistory />
+            <LoadingVacancyHistory />
+            <LoadingVacancyHistory />
+            <LoadingVacancyHistory />
+            <LoadingVacancyHistory />
+            <LoadingVacancyHistory />
+            <LoadingVacancyHistory />
+        </Fragment>
+    );
+};
+
+export default SkeletonVacancyHistory;
