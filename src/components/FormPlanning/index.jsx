c266b40387790fd480fd59d951350b8332f020b8
Leonardo Espinoza <leoespinozidk@gmail.com>
2022-02-21 21:57:09 -0600

    feat: form refactor to send data to our API

--- src/components/FormPlanning/index.jsx
+++ src/components/FormPlanning/index.jsx
@@ -1,123 +1,162 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
+import { useAuth0 } from '@auth0/auth0-react';
 
 import { Box, TextField, Select, InputLabel, MenuItem, FormControl, Button } from '@mui/material';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
 import TimePicker from '@mui/lab/TimePicker';
-import { getInterviews } from 'Api/Interviews/allInterviews';
 import { postNewInterview } from 'Api/Interviews/postInterview';
-import useFetch from 'Hooks/useFetch';
+import { useNavigate } from 'react-router-dom';
 
 export const FormPlanning = () => {
-    const { register, handleSubmit, control } = useForm();
-    const { data } = useFetch(getInterviews);
-    console.log(data);
-    const onSubmit = (interviewInfo) => postNewInterview(interviewInfo);
+    const { handleSubmit, control } = useForm();
+    const navigate = useNavigate();
+    const { user } = useAuth0();
+    const onSubmit = (dataForm) => {
+        // eslint-disable-next-line no-param-reassign
+        dataForm.user_id = user.sub;
+        postNewInterview(dataForm);
+        navigate('/interviews');
+    };
+
     return (
-        <Box component="form" onSubmit={handleSubmit(onSubmit)}>
-            <TextField
-                id="outlined-basic"
-                label="Interview title"
-                variant="outlined"
-                value=""
-                sx={{ width: '100%', mb: 2 }}
+        <Box component="form">
+            <Controller
+                name="interviewTitle"
+                defaultValue=""
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                    <TextField
+                        onChange={onChange}
+                        value={value}
+                        label="Interview title"
+                        variant="outlined"
+                        sx={{ width: '100%', mb: 2 }}
+                    />
+                )}
                 required
             />
             <Box sx={{ mb: 2 }}>
                 <LocalizationProvider dateAdapter={AdapterDateFns} sx={{ mr: 1 }}>
-                    <DatePicker
-                        label="Choose date"
+                    <Controller
+                        name="date"
+                        defaultValue=""
                         control={control}
-                        value=""
-                        {...register('date', { required: true })}
-                        renderInput={(params) => <TextField {...params} />}
+                        render={({ field: { onChange, value } }) => (
+                            <DatePicker
+                                label="Choose date"
+                                onChange={onChange}
+                                value={value}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        )}
+                        required
                     />
                 </LocalizationProvider>
                 <LocalizationProvider dateAdapter={AdapterDateFns} sx={{ mr: 1 }}>
-                    <TimePicker
-                        label="Choose hour"
+                    <Controller
+                        name="hour"
+                        defaultValue=""
                         control={control}
-                        value
-                        {...register('hour', { required: true })}
-                        renderInput={(params) => <TextField {...params} />}
+                        render={({ field: { onChange, value } }) => (
+                            <TimePicker
+                                label="Choose hour"
+                                onChange={onChange}
+                                value={value}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        )}
+                        required
                     />
                 </LocalizationProvider>
                 <FormControl sx={{ width: 200 }}>
                     <InputLabel id="select-interview-type">Interview type</InputLabel>
-                    <Select
-                        labelId="select-interview-type"
-                        id="interview-select"
-                        value=""
+                    <Controller
+                        name="interviewType"
+                        defaultValue=""
                         control={control}
-                        {...register('type', { required: true })}
-                        label="Interview type"
-                    >
-                        <MenuItem value="remote">Remote</MenuItem>
-                        <MenuItem value="face-to-face">Face-to-face</MenuItem>
-                    </Select>
+                        render={({ field: { onChange, value } }) => (
+                            <Select label="Interview type" onChange={onChange} value={value}>
+                                <MenuItem value="remote">Remote</MenuItem>
+                                <MenuItem value="face-to-face">Face-to-face</MenuItem>
+                            </Select>
+                        )}
+                        required
+                    />
                 </FormControl>
             </Box>
             <FormControl sx={{ width: '50%', mb: 2 }}>
                 <InputLabel id="platform">Platform</InputLabel>
-                <Select
-                    labelId="platform"
-                    id="platform-select"
-                    value=""
+                <Controller
+                    name="platform"
+                    defaultValue=""
                     control={control}
-                    {...register('platform', { required: true })}
-                    onChange={(e) => {
-                        e.target.innerText = e.target.value;
-                    }}
-                    label="Interview type"
-                >
-                    <MenuItem value="zoom">Zoom</MenuItem>
-                    <MenuItem value="google-meet">Google meet</MenuItem>
-                    <MenuItem value="microsoft-teams">Microsoft teams</MenuItem>
-                </Select>
+                    render={({ field: { onChange, value } }) => (
+                        <Select onChange={onChange} value={value} control={control} label="Interview type">
+                            <MenuItem value="zoom">Zoom</MenuItem>
+                            <MenuItem value="google-meet">Google meet</MenuItem>
+                            <MenuItem value="microsoft-teams">Microsoft teams</MenuItem>
+                        </Select>
+                    )}
+                    required
+                />
             </FormControl>
             <Box sx={{ mb: 2 }}>
-                <TextField
-                    id="outlined-basic"
-                    value=""
+                <Controller
+                    name="direction"
+                    defaultValue=""
                     control={control}
-                    {...register('direction', { required: true })}
-                    label="Url or phisical direction"
-                    variant="outlined"
-                    sx={{ width: '100%' }}
+                    render={({ field: { onChange, value } }) => (
+                        <TextField
+                            onChange={onChange}
+                            value={value}
+                            label="Url or phisical direction"
+                            variant="outlined"
+                            sx={{ width: '100%' }}
+                        />
+                    )}
+                    required
                 />
             </Box>
             <Box sx={{ mb: 2 }}>
                 <FormControl sx={{ width: '100%' }}>
                     <InputLabel id="interviewer">Who is going to interview?</InputLabel>
-                    <Select
-                        labelId="interviewer"
-                        value=""
+                    <Controller
+                        name="person"
+                        defaultValue=""
                         control={control}
-                        id="interviewer-select"
-                        label="Interview type"
-                        {...register('interviewer', { required: true })}
-                    >
-                        <MenuItem value="1">Person 1</MenuItem>
-                        <MenuItem value="2">Person 2</MenuItem>
-                        <MenuItem value="3">Person 3</MenuItem>
-                    </Select>
+                        render={({ field: { onChange, value } }) => (
+                            <Select onChange={onChange} value={value} label="Interview type">
+                                <MenuItem value="1">Person 1</MenuItem>
+                                <MenuItem value="2">Person 2</MenuItem>
+                                <MenuItem value="3">Person 3</MenuItem>
+                            </Select>
+                        )}
+                        required
+                    />
                 </FormControl>
             </Box>
-            <TextField
-                id="outlined-multiline-static"
-                value=""
+            <Controller
+                name="notes"
+                defaultValue=""
                 control={control}
-                {...register('notes', { required: true })}
-                label="Interview description"
-                multiline
-                rows={4}
-                sx={{ mb: 2, width: '100%' }}
+                render={({ field: { onChange, value } }) => (
+                    <TextField
+                        onChange={onChange}
+                        value={value}
+                        label="Interview description"
+                        multiline
+                        rows={4}
+                        sx={{ mb: 2, width: '100%' }}
+                    />
+                )}
+                required
             />
             <Box>
-                <Button variant="contained" type="submit">
+                <Button variant="contained" type="submit" onClick={handleSubmit(onSubmit)}>
                     Create
                 </Button>
             </Box>
