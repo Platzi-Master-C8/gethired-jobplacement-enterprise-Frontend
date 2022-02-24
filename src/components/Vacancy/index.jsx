c92e7f766c9046644b6adee13a770ab05ee4bcd6
Fernando De Leon <gasnare@hotmail.com>
2022-02-23 23:28:41 -0500

    refactor: correction of the cards, unify styles

--- src/components/Vacancy/index.jsx
+++ src/components/Vacancy/index.jsx
@@ -2,22 +2,10 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useAuth0 } from '@auth0/auth0-react';
 
-import { Button, FormControlLabel, Switch } from '@mui/material';
+import { Button, Card, FormControlLabel, Grid, Switch } from '@mui/material';
 import { inactiveVacancy } from '../../api/Vacancies/inactiveVacancy';
 import { activeVacancy } from '../../api/Vacancies/activeVacancy';
-import {
-    Vacancy as Container,
-    Header,
-    Div,
-    DivRight,
-    Tag,
-    Salary,
-    TagContainer,
-    Title,
-    Body,
-    Footer,
-    LinkStyled,
-} from './styles';
+import { Tag, Salary, Title, Body, LinkStyled } from './styles';
 
 export const Vacancy = ({ title, checked, salary, modality, applies, description, id }) => {
     const [status, setStatus] = useState(checked);
@@ -34,42 +22,52 @@ export const Vacancy = ({ title, checked, salary, modality, applies, description
     };
 
     return (
-        <Container>
-            <Header>
-                <Div>
+        <Card sx={{ p: 2, boxShadow: 3, mt: 2 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={8} sm={10} md={10}>
                     <Title>{title}</Title>
-                    <TagContainer>
-                        {isAuthenticated && (
-                            <FormControlLabel
-                                sx={{ mr: 2 }}
-                                control={<Switch checked={status} onChange={handleStatus} />}
-                                label="Vacancy active"
-                            />
-                        )}
-                        <Tag>{applies} applied </Tag>
-                    </TagContainer>
-                </Div>
-                <DivRight>
+                </Grid>
+                <Grid item xs={4} sm={2} md={2}>
                     <Salary>$ {Intl.NumberFormat().format(salary)}</Salary>
+                </Grid>
+                <Grid item xs={2}>
+                    {isAuthenticated && (
+                        <FormControlLabel
+                            sx={{ mr: 2 }}
+                            control={<Switch checked={status} onChange={handleStatus} />}
+                            label="Vacancy active"
+                        />
+                    )}
+                </Grid>
+                <Grid item xs={8}>
+                    <Tag>{applies} applied </Tag>
+                </Grid>
+                <Grid item xs={2}>
                     <Tag>{modality}</Tag>
-                </DivRight>
-            </Header>
-            <Body sx={{ m: 5 }}>{description}</Body>
-            {isAuthenticated && (
-                <Footer>
-                    <Button type="button" variant="contained">
-                        <LinkStyled color="#FFF" to={`/vacancies/${id}/edit`}>
-                            Edit
-                        </LinkStyled>
-                    </Button>
-                    <Button type="button" variant="outlined">
-                        <LinkStyled color="rgb(25, 118, 210)" to={`/vacancies/${id}`}>
-                            See more
-                        </LinkStyled>
-                    </Button>
-                </Footer>
-            )}
-        </Container>
+                </Grid>
+                <Grid item xs={12}>
+                    <Body sx={{ m: 5 }}>{description}</Body>
+                </Grid>
+                {isAuthenticated && (
+                    <Grid item xs={10.5}>
+                        <Button type="button" variant="contained">
+                            <LinkStyled color="#FFF" to={`/vacancies/${id}/edit`}>
+                                Edit
+                            </LinkStyled>
+                        </Button>
+                    </Grid>
+                )}
+                {isAuthenticated && (
+                    <Grid item xs={1.5}>
+                        <Button type="button" variant="outlined">
+                            <LinkStyled color="rgb(25, 118, 210)" to={`/vacancies/${id}`}>
+                                See more
+                            </LinkStyled>
+                        </Button>
+                    </Grid>
+                )}
+            </Grid>
+        </Card>
     );
 };
 
