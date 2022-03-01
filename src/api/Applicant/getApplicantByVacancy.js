ed34940ca114fe2dfe1b9f00daa42bfbd1682587
Daniel <oscar.d.gonzalez@gmail.com>
2022-03-01 15:20:03 -0500

    fix: get api request refactor

--- src/api/Applicant/getApplicantByVacancy.js
+++ src/api/Applicant/getApplicantByVacancy.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axiosSecondClient from '../axiosSecondClient';
 
 export default async function getApplicantByVacancy(id) {
     try {
-        const response = await axios.get(`https://18.190.40.7/api/v1/vacancies/${id}/applicants`);
+        const response = await axiosSecondClient.get(`/vacancies/${id}/applicants`);
         return response.data;
     } catch (err) {
         console.log(err);
