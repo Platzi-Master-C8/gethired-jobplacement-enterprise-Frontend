997cbb7677f7154a79b23b7eb506014471e2fcc7
Leonardo Espinoza <leoespinozidk@gmail.com>
2022-03-01 13:01:57 -0600

    fix: deleting auth conditional

--- src/components/ListOfVacancies/index.jsx
+++ src/components/ListOfVacancies/index.jsx
@@ -2,40 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { Divider, Button } from '@mui/material';
 import { useAuth0 } from '@auth0/auth0-react';
 
+import getVacanciesByUser from 'Api/Vacancies/getVacanciesByUser';
+
 import { Container, Title, Header, LinkStyled } from './styles';
 import SkeletonVacancyHistory from '../SkeletonVacancyHistory';
 import { Vacancy } from '../Vacancy';
 
-import { getAllVacancy } from '../../api/Vacancies/allVacancies';
-import getVacanciesByUser from '../../api/Vacancies/getVacanciesByUser';
-
 export const ListOfVacancies = () => {
     const [data, setData] = useState();
-    const { isAuthenticated, user } = useAuth0();
+    const { user } = useAuth0();
 
     useEffect(() => {
-        if (isAuthenticated) {
-            getVacanciesByUser(user.sub).then((response) => {
-                setData(response);
-            });
-        } else {
-            getAllVacancy().then((response) => {
-                setData(response.data.data);
-            });
-        }
-    }, [isAuthenticated]);
+        getVacanciesByUser(user.sub).then((response) => {
+            setData(response);
+        });
+    }, []);
 
     return (
         <Container>
             <Header>
                 <Title>History</Title>
-                {isAuthenticated && (
-                    <Button type="button" variant="contained">
-                        <LinkStyled color="#FFF" to="/vacancies/create">
-                            Create new vacancy
-                        </LinkStyled>
-                    </Button>
-                )}
+                <Button type="button" variant="contained">
+                    <LinkStyled color="#FFF" to="/vacancies/create">
+                        Create new vacancy
+                    </LinkStyled>
+                </Button>
             </Header>
             {data?.length > 0 ? (
                 data.map((vacancy) => (
