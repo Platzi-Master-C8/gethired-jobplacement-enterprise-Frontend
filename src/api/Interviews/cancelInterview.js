ef84d021b26bd04ec0cf9871f473c772799b0204
Zajith Corro <zajithcorrov@hotmail.com>
2022-03-04 23:34:04 -0600

    fix: http method

--- src/api/Interviews/cancelInterview.js
+++ src/api/Interviews/cancelInterview.js
@@ -6,5 +6,5 @@ export const cancelInterview = ({ id, reason }) => {
         description: reason,
     };
 
-    return axiosClient.post(`/interviews/${id}/cancel`, data).then(console.log).catch(console.log);
+    return axiosClient.patch(`/interviews/${id}/cancel`, data).then(console.log).catch(console.log);
 };
