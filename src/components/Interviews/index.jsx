f75d0fde168f2c65f6833fc0aa378aa1ded1b128
Zajith Corro <zajithcorrov@hotmail.com>
2022-03-04 23:32:21 -0600

    fix: delete and fix names

--- src/components/Interviews/index.jsx
+++ src/components/Interviews/index.jsx
@@ -1,15 +1,13 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 
 import { CancelInterview } from 'Components/CancelInterviewModal';
 import { RescheduleInterview } from 'Components/RescheduleInterviewModal';
 import { InterviewNotification } from 'Components/InterviewNotificationModal';
 
-import { cancelInterview } from 'Api/interviews/cancelInterview';
+import { cancelInterview } from 'Api/Interviews/cancelInterview';
 import { getAllInterviews } from 'Api/Interviews/getAllInterviews';
 
 import useModal from 'Hooks/useModal';
-import useFetch from 'Hooks/useFetch';
 import { InterviewsList } from './InterviewsList';
 
 export const Interviews = () => {
